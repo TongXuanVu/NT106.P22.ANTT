fcf9bb1bb40c3529abee0e8723c769303a84069d
TongXuanVu <tongxuanvuk1920@gmail.com>
2025-05-13 09:50:33 +0700

    Add files via upload

---
+++ LANSPYproject/Device.cs
@@ -0,0 +1,11 @@
+﻿namespace LANSPYproject
+{
+    public class Device
+    {
+        public int ID { get; set; }
+        public string IP { get; set; }
+        public string MAC { get; set; }
+        public string Name { get; set; }
+        public string Date { get; set; } = System.DateTime.Now.ToString("dd/MM, hh:mm tt");
+    }
+}
--- LANSPYproject/Scanner.xaml.cs
+++ LANSPYproject/Scanner.xaml.cs
@@ -1,46 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace LANSPYproject
 {
-    /// <summary>
-    /// Interaction logic for Scanner.xaml
-    /// </summary>
     public partial class Scanner : UserControl
     {
-        public class Device
-        {
-            public int ID { get; set; }
-            public string IP { get; set; }
-            public string MAC { get; set; }
-            public string Name { get; set; }
-            // Date sẽ luôn được set theo thời gian hiện tại khi tạo mới
-            public string Date { get; set; } = DateTime.Now.ToString("dd/MM, hh:mm tt");
-        }
+        public ObservableCollection<Device> Devices { get; set; } = new ObservableCollection<Device>();
 
         public Scanner()
         {
             InitializeComponent();
-
-            List<Device> devices = new List<Device>
-        {
-            new Device { ID = 1, IP = "192.168.1.10", MAC = "2C:54:91:88:C9:E3", Name = "hp el g4", Date = "25/03, 10:40 PM" },
-            new Device { ID = 2, IP = "192.168.1.8", MAC = "D0:38:DC:3C:5B:48", Name = "Thinkpad x240", Date = "25/03, 10:40 PM" }
-        };
-            
-            //deviceDataGrid.ItemsSource = devices;
+            deviceDataGrid.ItemsSource = Devices;
         }
-    }
 
+        private void ScanButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Đã bấm nút quét!");
+            Devices.Clear();
+            ScanNetwork();
+        }
+
+        private async void ScanNetwork()
+        {
+            string baseIP = "192.168.1.";
+
+            for (int i = 1; i < 255; i++)
+            {
+                string ip = baseIP + i;
+                try
+                {
+                    await new Ping().SendPingAsync(ip, 300);
+                }
+                catch { }
+            }
+
+            await Task.Delay(2000); // Đợi bảng ARP cập nhật
+
+            var arp = new ProcessStartInfo
+            {
+                FileName = "arp",
+                Arguments = "-a",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            var process = Process.Start(arp);
+            string output = process.StandardOutput.ReadToEnd();
+
+            var lines = output.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            int id = 1;
+            foreach (var line in lines)
+            {
+                var match = Regex.Match(line, @"(\d+\.\d+\.\d+\.\d+)\s+([a-fA-F0-9:-]{17})");
+                if (match.Success)
+                {
+                    string ip = match.Groups[1].Value;
+                    string mac = match.Groups[2].Value;
+                    Devices.Add(new Device
+                    {
+                        ID = id++,
+                        IP = ip,
+                        MAC = mac,
+                        Name = "Unknown",
+                        Date = DateTime.Now.ToString("dd/MM, hh:mm tt")
+                    });
+                }
+            }
+        }
+
+    }
 }
