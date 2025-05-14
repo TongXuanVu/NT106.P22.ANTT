b5109ddc55bd9513954a5bc5f5d0861c70704513
TongXuanVu <tongxuanvuk1920@gmail.com>
2025-05-14 10:14:28 +0700

    Add files via upload
    maincontent

--- LANSPYproject/Lanspy.xaml.cs
+++ LANSPYproject/Lanspy.xaml.cs
@@ -1,51 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace LANSPYproject
 {
-    /// <summary>
-    /// Interaction logic for Window1.xaml
-    /// </summary>
     public partial class Lanspy : Window
     {
         public Lanspy()
         {
-
+            InitializeComponent();
+            // Mặc định mở trang Dashboard khi khởi động
+            MainContent.Content = new Dashboard();
         }
 
         private void DashboardButton_Click(object sender, RoutedEventArgs e)
         {
-            MainFrame.Navigate(new Dashboard());
+            MainContent.Content = new Dashboard();
         }
+
         private void ScannerButton_Click(object sender, RoutedEventArgs e)
         {
-            MainFrame.Navigate(new Scanner());
+            MainContent.Content = new Scanner();
         }
 
         private void LogsButton_Click(object sender, RoutedEventArgs e)
         {
-            MainFrame.Navigate(new Logs());
+            MainContent.Content = new Logs();
         }
 
         private void AlertsButton_Click(object sender, RoutedEventArgs e)
         {
-            MainFrame.Navigate(new Alerts());
+            MainContent.Content = new Alerts();
         }
 
         private void SettingButton_Click(object sender, RoutedEventArgs e)
         {
-            MainFrame.Navigate(new Setting());
+            MainContent.Content = new Setting();
         }
     }
 }
