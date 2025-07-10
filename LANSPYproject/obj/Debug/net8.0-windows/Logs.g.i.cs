a09da76dc121c796ee1fd0c93d293c7f86e39931
TongXuanVu <tongxuanvuk1920@gamil.com>
2025-07-10 14:58:00 +0700

    deloy sql

--- LANSPYproject/obj/Debug/net8.0-windows/Logs.g.i.cs
+++ LANSPYproject/obj/Debug/net8.0-windows/Logs.g.i.cs
@@ -116,7 +116,7 @@ namespace LANSPYproject {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/LANSPYproject;V1.0.0.0;component/logs.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/LANSPYproject;component/logs.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Logs.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
